060dae06364b2f467c7236ac663c8a75089a0770
gurumobile <bulitnoveler@outlook.com>
2017-05-17 20:30:28 +0300

    AVFoundation framework use

---
+++ QRCodeReader/QRCodeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  QRCodeViewController.swift
+//  QRCodeReader
+//
+//  Created by Sambo on 13/05/2017.
+//  Copyright © 2017 Sambo. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
